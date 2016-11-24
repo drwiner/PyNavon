4a15c066b1380a49c50f033eda42d8e1a14bacc4
drwiner <drwiner131@gmail.com>
2016-11-23 22:58:28 -0500

    its transforming

--- testing_vertexdrawing.py
+++ testing_vertexdrawing.py
@@ -7,27 +7,33 @@ import time
 import math
 
 
-Pattern = namedtuple('Pattern', ['length', 'positions'])
+Pattern = namedtuple('Pattern', ['length', 'positions', 'center'])
 
-pat_A = Pattern(12,positions=np.array([(5,0),(6,0),(5,1),(6,1),(4,2),(5,2),(6,2),(7,2),(4,3),(5,3),(6,3),(7,3),(3,4),(4,4),(7,4),(8,4),(3,5),(4,5),(7,5),(8,5),(2,6),(3,6),(4,6),(5,6),(6,6),(7,6),(8,6),(9,6),(2,7),(3,7),(4,7),(5,7),(6,7),(7,7),(8,7),(9,7),(1,8),(2,8),(9,8),(10,8),(1,9),(2,9),(9,9),(10,9),(0,10),(1,10),(10,10),(11,10),(0,11),(1,11),(10,11),(11,11)]))
+pat_A = Pattern(length=12,
+                positions=np.array([(5,0),(6,0),(5,1),(6,1),(4,2),(5,2),(6,2),(7,2),(4,3),(5,3),(6,3),(7,3),(3,4),(4,4),(7,4),(8,4),(3,5),(4,5),(7,5),(8,5),(2,6),(3,6),(4,6),(5,6),(6,6),(7,6),(8,6),(9,6),(2,7),(3,7),(4,7),(5,7),(6,7),(7,7),(8,7),(9,7),(1,8),(2,8),(9,8),(10,8),(1,9),(2,9),(9,9),(10,9),(0,10),(1,10),(10,10),(11,10),(0,11),(1,11),(10,11),(11,11)]),
+                center=(5, 6))
 
 # _,pos = pat_A
 # cell_A = np.array([(i, 1200-j) for i, j in pos])
 
 
-pat_C = Pattern(12,positions=np.array([(0,4),(0,5),(0,6),(0,7),(1,3),(1,4),(1,5),(1,6),(1,7),(1,8),(2,2),(2,3),(2,4),(2,7),(2,8),(2,9),(3,1),(3,2),(3,3),(3,8),(3,9),(3,10),(4,0),(4,1),(4,2),(4,9),(4,10),(4,11),(5,0),(5,1),(6,0),(6,1),(7,0),(7,1),(8,0),(8,1),(5,11),(5,10),(6,11),(6,10),(7,11),(7,10),(8,11),(8,10),(9,11),(9,10),(9,9),(10,10),(10,9),(10,8),(9,0),(9,1),(9,2),(9,3),(9,8),(10,1),(10,2),(10,3)]))
+pat_C = Pattern(length=12,
+                positions=np.array([(0,4),(0,5),(0,6),(0,7),(1,3),(1,4),(1,5),(1,6),(1,7),(1,8),(2,2),(2,3),(2,4),(2,7),(2,8),(2,9),(3,1),(3,2),(3,3),(3,8),(3,9),(3,10),(4,0),(4,1),(4,2),(4,9),(4,10),(4,11),(5,0),(5,1),(6,0),(6,1),(7,0),(7,1),(8,0),(8,1),(5,11),(5,10),(6,11),(6,10),(7,11),(7,10),(8,11),(8,10),(9,11),(9,10),(9,9),(10,10),(10,9),(10,8),(9,0),(9,1),(9,2),(9,3),(9,8),(10,1),(10,2),(10,3)]),
+                center=(3, 8))
 
 # _,pos = pat_C
 # cell_C = np.array([(i,1200-j) for i,j in pos])
 
 
-pat_T = Pattern(12,positions=np.array([(1,0),(2,0),(3,0),(4,0),(5,0),(6,0),(7,0),(8,0),(9,0),(10,0),(1,1),(2,1),(3,1),(4,1),(5,1),(6,1),(7,1),(8,1),(9,1),(10,1),(1,2),(10,2),(5,2),(5,3),(5,4),(5,5),(5,6),(5,7),(5,8),(5,9),(5,10),(6,2),(6,3),(6,4),(6,5),(6,6),(6,7),(6,8),(6,9),(6,10),(3,10),(4,9),(4,10),(7,9),(7,10),(8,10)]))
+pat_T = Pattern(length=12,
+                positions=np.array([(1,0),(2,0),(3,0),(4,0),(5,0),(6,0),(7,0),(8,0),(9,0),(10,0),(1,1),(2,1),(3,1),(4,1),(5,1),(6,1),(7,1),(8,1),(9,1),(10,1),(1,2),(10,2),(5,2),(5,3),(5,4),(5,5),(5,6),(5,7),(5,8),(5,9),(5,10),(6,2),(6,3),(6,4),(6,5),(6,6),(6,7),(6,8),(6,9),(6,10),(3,10),(4,9),(4,10),(7,9),(7,10),(8,10)]),
+                center=(5, 5))
 
 # _,pos = pat_T
 # cell_T = np.array([(i,1200-j) for i,j in pos])
 
 
-pat_test = Pattern(2, positions=np.array([(0,0), (1,1)]))
+#pat_test = Pattern(2, positions=np.array([(0,0), (1,1)]))
 
 word = [pat_T, pat_C, pat_A]
 #cell_worlds = [cell_T, cell_C, cell_A]
@@ -35,7 +41,6 @@ word = [pat_T, pat_C, pat_A]
 class Cell:
 	def __init__(self, numpy_coord, size):
 		self.coordinate = numpy_coord
-		#self.position = numpy_position
 		self.size = size
 
 	def get_cells(self):
@@ -48,38 +53,29 @@ class Host:
 		self.levels = levels
 		self.top_level = 0
 		self.pointsize = 1.0
-		#self.indices = self.recursive_strategy()
 		self.updateField()
 
 	#@clock
 	def grow(self):
-		self.coordinate[0] -= 25
+		#self.coordinate[0] -= 25
+		b4 = self.coordinate
 		self.size += (self.size / 16)
 
 		if self.pointsize > 5.0:
 			self.pointsize -= 3.0
 
-		#if self.coordinate[0] < -1200:
-		#	self.coordinate[0] = 1200
 		self.updateField()
+
+		#cb = self.coordinate - b4
+		#self.coordinate += cb
+		L, P, C = self.getWord(self.top_level + 1)
+		self.dialate(C, self.size, L)
+
 		self.pointsize += .13
-		print('pointsize', self.pointsize)
-		print('size', self.size)
+		# print('pointsize', self.pointsize)
+		# print('size', self.size)
 		if self.size > 8000:
 			self.expand()
-
-		# self.size += (self.size/36)
-		# self.coordinate[0] -= 15
-		# self.coordinate[1] -= 15
-		# self.updatePointSize()
-		#self.updateField()
-
-	def updatePointSize(self, n=.35):
-		self.pointsize += n
-		if self.pointsize < .5:
-			self.pointsize = 10.0
-			#self.pointsize = .5
-
 
 	def make_cells(self, t, i, length, patt):
 		size = int(self.size / math.pow(length, i))
@@ -92,12 +88,22 @@ class Host:
 
 	def top_range(self):
 		if self.top_level == 0:
-			length, patt = word[-1]
+			length, patt, _ = word[-1]
 		else:
-			length, patt = word[self.top_level-1]
+			length, patt, _ = word[self.top_level-1]
 		size = int(self.size / math.pow(length, 1))
 		return self.coordinate + patt * (size / length)
 
+	def dialate(self, ctr, size, length):
+		#np.array(ctr)*(self.size / 16)
+
+		c = self.coordinate + np.array(ctr)*(size/length)
+		c = c/16
+		c = self.coordinate - c
+		self.coordinate = c
+
+	def getWord(self, i):
+		return word[((self.top_level + i) % len(word))]
 
 	def make_top_lefts(self, t, i, length, patt):
 		if i >= self.levels:
@@ -105,8 +111,7 @@ class Host:
 
 		size = int(self.size / math.pow(length, i))
 		p_1 = t + patt * (size / length)
-		#print(((i+1) % len(word)))
-		ll, pp = word[((self.top_level + i+1) % len(word))]
+		ll, pp, _ = self.getWord(i+1)
 
 		cells = []
 		mtl = partial(self.make_top_lefts, i=i+1, length=ll, patt=pp)
@@ -114,18 +119,20 @@ class Host:
 			cells.extend(mtl(tt))
 		return cells
 
-	#@clock
 	def recursive_strategy(self):
-		length_0, patt_0 = word[self.top_level]
+		length_0, patt_0, _ = word[self.top_level]
 		return self.make_top_lefts(self.coordinate, 0, length_0, patt_0)
 
-	@clock
 	def f(self, field):
 		return [field[j][i] for i, j in self.indices]
 
 	@clock
 	def updateField(self):
+		#before_coordinate = self.coordinate
 		self.field = np.array(self.recursive_strategy())
+		#cb = self.coordinate - before_coordinate
+		#self.coordinate += cb
+
 
 	def old_update_field(self):
 		# idea: generate incides once (and after every expand), and just generate this array. But, not as fast apprtly.
@@ -135,22 +142,13 @@ class Host:
 		self.field = np.array(self.f(field))
 
 	def expand(self):
-		#we'll need to know which cell is/ to make the new head.
-		#Take the range of the top level again
 		self.top_level += 1
 		if self.top_level >= len(word):
 			self.top_level = 0
 		top_lefts = self.top_range()
 
-		#get the distnace of each top_level from 0,0
 		#self.coordinate = find_nearest(top_lefts, np.zeros_like(top_lefts))
 		self.size = self.size/12
-		#now the coordinate of interest in this one which is closest.
-
-		pass #self.length = self.length*self.length
-
-	def contract(self):
-		pass
 
 def initial_config():
 	C = Cell(np.array([0, 0]), 1200)
